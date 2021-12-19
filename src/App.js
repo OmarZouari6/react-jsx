e8021eeee62bf842ea86e89248dc3c335931a1a3
OmarZouari6 <zouari6omar@gmail.com>
2021-12-19 20:32:17 +0100

    omarto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import image from "../src/public.png";
+import image1 from "../src/imageInSrc.jpg";
+import video from "./video.mp4";
+import "./style.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">Omar Zouari</h1>
+        <br />
+        <img className="image1" src={image1} alt="" />
+        <br />
+        <img className="image2" src={image} alt="" />
+      </div>
+      <video width={320} height={240} controls>
+        <source src={video} type="video/mp4" />
+      </video>
     </div>
   );
 }
